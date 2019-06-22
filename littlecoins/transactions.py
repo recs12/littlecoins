9edc90799b65aa6a88d124e774be2e56340de021
unknown <slimane.rechdi@gmail.com>
2019-06-21 22:16:02 -0400

    bug fixed

--- littlecoins/transactions.py
+++ littlecoins/transactions.py
@@ -1,108 +1,151 @@
 #!python3
 # coding: utf-8
-"""
-Place all the .csv files into a DataFrame 
-"""
 
 import re
 import os
 import glob
 import numpy as np
-import pandas as pd
 import sys
+import loguru
+import pandas as pd
+pd.set_option('mode.chained_assignment', None)
 
-def list_of_files(location='.', suffixe='.csv'):
-    """Get a list of file names in the current location.
+def list_of_files(location="."):
+    """return a list of csv files."""
+    return [csv for csv in os.listdir(location) if csv.endswith(".csv")]
 
-    Parameters
-    ----------
-    location : str (default local)
-        The file location of the files.csv
-    suffixe : str (default='.csv')
-
-    Returns
-    -------
-    list
-            list_of_files('.') ->  ['file1.csv', 'file2.csv' , 'file3.csv']
-    """
-    return [files for files in os.listdir(location) if files.endswith(suffixe)]
 
 # Create a function to process all of the csv files
 def read_file_csv(file_csv):
-        """read .csv file -> dataframe with columns names added"""
-        list_cols = ['City','Acc.','Type','date_transaction','num','Description','Cheque','Expenses','Incomes','NoK1','NoK2','NoK3','NoK4','Cum.']
-        df = pd.read_csv(file_csv, header=None, names = list_cols , encoding='latin1')
-        return df
+    """Read .csv file -> dataframe with columns names added"""
+    list_cols = [
+        "City",
+        "Acc.",
+        "Type",
+        "date_transaction",
+        "num",
+        "Description",
+        "Cheque",
+        "Expenses",
+        "Incomes",
+        "NoK1",
+        "NoK2",
+        "NoK3",
+        "NoK4",
+        "Cum.",
+    ]
+    df = pd.read_csv(file_csv, header=None, names=list_cols, encoding="latin1")
+    return df
+
 
 def card_debit():
-    #Create a list of dataframes
-    list_to_concat = [ read_file_csv(df_name)for df_name in list_of_files()]
-    df_total = pd.concat(list_to_concat, sort=False)
-    # #Clean the datas
-    df_total['date_transaction'] = pd.to_datetime(df_total['date_transaction'], format="%d/%m/%Y" , errors="ignore")
-    df_index_by_dates = df_total.set_index(df_total['date_transaction'])
-    transactions = df_index_by_dates[['Acc.', 'date_transaction' , 'Description', 'Cheque','Expenses', 'Incomes']]
-    transactions = transactions.dropna(how='all')
-    transactions.loc[:,'card'] = 'debit'
-    return transactions
+    """Merge all the csv file into one dataframe with all the debit transactions."""
+    csv_serie = [read_file_csv(df_name) for df_name in list_of_files()]
+    debit = pd.concat(csv_serie, sort=False)
+    debit["date_transaction"] = pd.to_datetime(
+        debit["date_transaction"], format="%Y/%m/%d", errors="ignore"
+    )
+    debit = debit.set_index(debit["date_transaction"])
+    debit = debit[
+        ["Acc.", "date_transaction", "Description", "Cheque", "Expenses", "Incomes"]
+    ]
+    debit = debit.dropna(how="all")
+    debit.loc[:, "card"] = "debit"
+    return debit
+
 
 # ********************************************************
 
-# CC = pd.read_csv("CC_transactions.csv")
-def grapping_txt_files_list(location, suffixe='.txt'): # prefixe=None, middle='######'
-    """bla bla bla"""
-    return [i for i in os.listdir(location) if i.endswith(suffixe)==True]
+def grapping_txt_files_list(location):
+    """Make a list of the text documents in the folder."""
+    credit = [txt for txt in os.listdir(location) if txt.endswith(".txt")]
+    if not credit:
+        return None
+    return credit
+
 
 def scraping_txt_file(filetxt):
-    """bla bla bla"""
+    """This scrape the data inside text files using regular expression. """
     datum = []
-    with open( filetxt , 'r', encoding='latin1') as foo:
-        for line in foo: #I think a genertator is imaginable here!
+    with open(filetxt, "r", encoding="latin1") as foo:
+        for line in foo:  # I think a genertator is imaginable here!
             pattern = re.compile(
-                                r"(?P<Date_de_transaction>\d{2} .{3} \d{4})\t*" \
-                                r"(?P<Date_inscription>\d{2} .{3} \d{4})\t*"\
-                                r"(?P<Transaction>\d{3})\t{3}"\
-                                r"(?P<data4>.*|\s)\t{1}"\
-                                r"(?P<Montant>\d{1,4},\d{2})"
-                                )
+                r"(?P<Date_de_transaction>\d{2}.\w{3}.\d{4})\t*"
+                r"(?P<Date_inscription>\d{2}.\w{3}.\d{4})\t*"
+                r"(?P<Transaction>\d{3})\t*"
+                r"(?P<data4>.*|\s)\t{1}"
+                r"(?P<Montant>\d{1,4}[,|.]\d{2})"
+            )
             matchObjects = re.search(pattern, line)
-            if matchObjects != None:
-                datum.append(matchObjects.groups()) # extract a group of dict
+            if matchObjects:
+                datum.append(matchObjects.groups())  # extract a group of dict
+    if not datum:
+        return None
     return datum
 
-def generate_df(file_name , data):
-    """bla bla bla"""
+
+def generate_df(file_name, data):
+    """Generate a dataframe attached to a name."""
     import csv
-    csv_name = str(os.path.splitext(file_name)[-2]) + '.csv' # replace .txt - > .csv
-    df = pd.DataFrame(data, columns=['date_transaction','Date_2','number','Description','Expenses'])
-    calendar = {'JAN':'01' , 'FÉV':'02' , 'MAR':'03' , 'AVR':'04' , 'MAI':'05', 'JUN':'06' , 'JUI':'07' , 'AOÛ':'08' , 'SEP':'09' , 'OCT':'10', 'NOV':'11', 'DÉC':'12'}
-    for month,number in calendar.items():
-        df['date_transaction'] = df['date_transaction'].str.replace(month,number)         
-    df['date_transaction'] = pd.to_datetime(df['date_transaction'] , format="%Y-%m-%d",errors='ignore') 
-    df = df.drop(['Date_2'], axis=1)
+    df = pd.DataFrame(
+        data,
+        columns=["date_transaction", "Date_2", "number", "Description", "Expenses"],
+    )
+    calendar = {
+        "JAN": "JAN",
+        "FÉV": "FEB",
+        "MAR": "MAR",
+        "AVR": "APR",
+        "MAI": "MAY",
+        "JUN": "JUN",
+        "JUI": "JUL",
+        "AOÛ": "AUG",
+        "SEP": "SEP",
+        "OCT": "OCT",
+        "NOV": "NOV",
+        "DÉC": "DEC",
+    }
+    for month, number in calendar.items():
+        df["date_transaction"] = df["date_transaction"].str.replace(month, number)
+    df["date_transaction"] = pd.to_datetime(
+        df["date_transaction"], format="%d %m %Y", errors="ignore"
+    )
+    df = df.drop(["Date_2"], axis=1)
     return df
+
 
 def card_credit():
-    df_list = [generate_df(fname, scraping_txt_file(fname)) for fname in grapping_txt_files_list('.')]
-    df_total = pd.concat(df_list,sort=False)
+    df_list = [
+        generate_df(fname, scraping_txt_file(fname))
+        for fname in grapping_txt_files_list(".")
+    ]
+    df_total = pd.concat(df_list, sort=False)
     REGX = r"^FRAIS.*DE.*CR.*IT"
-    frais = df_total[df_total['Description'].str.contains(REGX, regex=True).values]
-    frais.loc[frais.Description.str.contains(REGX, regex=True),'Description']= 'credit fees'  
-    frais = frais.dropna(how='all')
-    frais.loc[:,'card'] = 'credit'
-    print(frais.columns)
-    transactions = df_total[~df_total['Description'].str.contains(REGX).values]
-    transactions = transactions.dropna(how='all')
-    transactions.loc[:,'card'] = 'credit' 
-    df = pd.concat([transactions,frais], sort=False)
-    df['Expenses'] = df['Expenses'].str.replace(',','.') 
+    frais = df_total[df_total["Description"].str.contains(REGX, regex=True).values]
+    frais.loc[
+        frais.Description.str.contains(REGX, regex=True), "Description"
+    ] = "credit fees"
+    frais = frais.dropna(how="all")
+    frais.loc[:, "card"] = "credit"
+    transactions = df_total[~df_total["Description"].str.contains(REGX).values]
+    transactions = transactions.dropna(how="all")
+    transactions.loc[:, "card"] = "credit"
+    df = pd.concat([transactions, frais], sort=False)
+    df["Expenses"] = df["Expenses"].str.replace(",", ".")
     return df
 
-if __name__ == "__main__":
-    credit , debit = card_credit() , card_debit()
+
+def main():
+    credit, debit = card_credit(), card_debit()
     total = pd.concat([credit, debit], sort=False)
-    total['date_transaction'] = pd.to_datetime(total['date_transaction']).dt.strftime('%Y-%m-%d')
-    total = total.sort_values(by='date_transaction', ascending=False)
-    filename = 'total_transactions.csv'
+    total["date_transaction"] = pd.to_datetime(total["date_transaction"]).dt.strftime(
+        "%Y-%m-%d"
+    )
+    total = total.sort_values(by='date_transaction', ascending=True) 
+    filename = "transactions.csv"
     total.to_csv(filename, index=None)
-    print(f"-> {filename} created in the current folder. ")
+    print(f"\nReport >>> {filename}")
+
+
+if __name__ == "__main__":
+    main()
